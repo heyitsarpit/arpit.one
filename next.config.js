3456ca4e735d437573bd29f5a434a5ba619e4b54
Arpit Bharti <arpitbharti73@gmail.com>
2025-01-20 23:00:07 +0530

    update all packages and node version

--- next.config.js
+++ next.config.js
@@ -1,26 +1,5 @@
-const withPWA = require('next-pwa');
-const runtimeCaching = require('next-pwa/cache');
-
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: 'public',
-    runtimeCaching
-  },
+module.exports = {
   images: {
-    domains: ['images.pexels.com', 'user-images.githubusercontent.com']
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: '/bee.js',
-        destination: 'https://cdn.splitbee.io/sb.js'
-      },
-      {
-        source: '/_hive/:slug',
-        destination: 'https://hive.splitbee.io/:slug'
-      }
-    ];
+    domains: ['user-images.githubusercontent.com']
   }
-});
+};
